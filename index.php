bf0a4f4f0f7505d8d3f6356c3f6d27b9f3d95a9c
Saeed Mughal <saeedpucit.e@gmail.com>
2022-06-09 05:53:46 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+// $conn = new mysqli('localhost', 'root', '');
+
+// if (!$conn) {
+//     echo "Error Establishing Connection" . $conn->error;
+// }
+
+// $sql = "CREATE DATABASE lms";
+
+// if ($conn->query($sql)===true ) {
+//     echo "Database LMS Created";
+// }
+// $conn->close();
+
+// $conn = new mysqli('localhost', 'root', '', 'lms');
+
+// if (!$conn) {
+//     echo "Error Establishing Connection" . $conn->error;
+// }
+
+// $sql = "CREATE TABLE student (
+//     id INT(20) UNSIGNED PRIMARY KEY  AUTO_INCREMENT,
+//     sname VARCHAR(40) not null,
+//     srollno VARCHAR(40) NOT NULL
+// )";
+
+// if ($conn->query($sql)===true ) {
+//     echo "Table Student Created";
+// }
+// $conn->close();
+
+?>
